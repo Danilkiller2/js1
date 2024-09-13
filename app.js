7d8c683add20db2c99b3d036e6d7b56a9555a59a
Danilkiller2 <danilnew2008@gmail.com>
2024-09-13 10:58:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+alert("Lesson JS")
+document.write("somvsovm")
+console.log("bmsaedm")
+console.log (10+40)
+console.log(40-10)
+console.log(10*20)
+console.log(10/2)
+console.log(5**5)
+let num
+num = 123
+alert(num)
+let a, b, c;
+
+a = 1;
+b = 2;
+c = 3;
