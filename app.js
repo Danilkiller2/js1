3c8cd2481dd5c00eacd19ff16b01e85d0fe12cff
Danilkiller2 <danilnew2008@gmail.com>
2024-09-18 22:35:53 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-alert("Lesson JS")
-document.write("somvsovm")
-console.log("bmsaedm")
-console.log (10+40)
-console.log(40-10)
-console.log(10*20)
-console.log(10/2)
-console.log(5**5)
-let num
-num = 123
-alert(num)
-let a, b, c;
-
-a = 1;
-b = 2;
-c = 3;
+let year = 365
+let day = 30
+let hour = 24
+let minute = 60
+let sec = 60
+console.log(`в сутках ${hour*minute*sec} секунд`)
+console.log(`в 30 сутках ${days*hour*minute*sec} секунд`)
+console.log(`в году ${year*hour*minute*sec} секунд`)
+console.log(`в сутках ${hour*minute} минут`)
+console.log(`в году ${year*hour*minute} минут`)
+let onekb = 1
+let кб = 1024
+let мб = 1024
+let гб = 1024
+let tengb = 1024*10
+let tb = 1024
+console.log(`в мегабайте ${bait*kb*mb} байт`)
+console.log(`в гигабайте ${bait*kb*mb*gb} байт`)
+console.log(`в 10 гигабайтах ${bait*kb*mb*tengb} байт`)
+console.log(`в терабайте ${bait*kb*mb*gb*tb} байт`)
+console.log(`в терабайте ${onekb*mb*gb*tb} килобайт`)
