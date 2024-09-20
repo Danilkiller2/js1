dd38b4903f7c07f4c70064f7b4935d54f5d62946
Danilkiller2 <danilnew2008@gmail.com>
2024-09-20 12:59:48 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,67 @@
-let year = 365
-let day = 30
-let hour = 24
-let minute = 60
-let sec = 60
-console.log(`в сутках ${hour*minute*sec} секунд`)
-console.log(`в 30 сутках ${days*hour*minute*sec} секунд`)
-console.log(`в году ${year*hour*minute*sec} секунд`)
-console.log(`в сутках ${hour*minute} минут`)
-console.log(`в году ${year*hour*minute} минут`)
-let onekb = 1
-let кб = 1024
-let мб = 1024
-let гб = 1024
-let tengb = 1024*10
-let tb = 1024
-console.log(`в мегабайте ${bait*kb*mb} байт`)
-console.log(`в гигабайте ${bait*kb*mb*gb} байт`)
-console.log(`в 10 гигабайтах ${bait*kb*mb*tengb} байт`)
-console.log(`в терабайте ${bait*kb*mb*gb*tb} байт`)
-console.log(`в терабайте ${onekb*mb*gb*tb} килобайт`)
+console.log(`Задание 1`)
+
+let mounth = //prompt(`Введите число от 1 до 12`);
+
+mounths = [`январь`, `февраль`, `март`, `апрель`, `май`, `июнь`, `июль`, `август`, `сентябрь`, `октябрь`, `ноябрь`, `декабрь`]
+
+console.log(mounths[mounth-1]);
+
+
+console.log(`Задание 2`)
+let dostavka = 250;
+
+let tovar1 = [15000, `Процессор`, dostavka];
+let tovar2 = [25000, `Видеокарта`, dostavka];
+let tovar3 = [5000, `Блок питания`, dostavka];
+
+let sum = tovar1[2]+tovar2[2]+tovar3[2]+tovar1[0]+tovar2[0]+tovar3[0]
+
+console.log(`Общая сумма товара равна ${sum}`);
+
+console.log(`Задание 3`)
+
+let arr = [25, 30, 6, 9, 8]
+
+console.log(`Максимальное число из массива [25, 30, 6, 9, 8] равно ${Math.max(...arr)}`)
+console.log(`Максимальное число из массива [25, 30, 6, 9, 8] равно ${Math.min(...arr)}`)
+
+console.log(`Задание 4`)
+
+arr = [
+    Math.floor(Math.random()*10),
+    Math.floor(Math.random()*10),
+    Math.floor(Math.random()*10),
+    Math.floor(Math.random()*10),
+    Math.floor(Math.random()*10)
+]
+console.log(arr)
+console.log(arr.sort())
+console.log(arr.sort().reverse())
+
+
+console.log(`Задание 5`)
+arr = ["Hi", "Hello", "Bonjour"];
+console.log(arr)
+arr.push('Hola')
+console.log(arr)
+
+console.log(`Заданиие 6`)
+let a = ['a', 'b', 'c'];
+let b = [1, 2, 3];
+console.log(`Массив a = ${a}`)
+console.log(`Массив b = ${b}`)
+console.log(a.concat(b))
+
+console.log(`Задание 7`)
+
+arr = [1, 2, 3]
+console.log(arr)
+console.log(arr.reverse())
+
+console.log(`Задание 8`)
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.includes(`Banana`))
+
+console.log(`Задание 9`)
+const array =[2, 5, 9];
+console.log(array)
